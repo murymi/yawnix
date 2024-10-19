09194751dbccb3280a084557bb67d24662ce97ba
vic <murimimlvictor@gmail.com>
2024-10-19 17:28:38 +0300

    pic driver

--- src/drivers/pic.c
+++ src/drivers/pic.c
@@ -10,30 +10,42 @@ void pic_clear_mask(unsigned char idx)
 }
 
 void pic_remap() {
-    pic_write_master(0x10 | 0x01);
+    pic_write_cmd_master(0x10 | 0x01);
     io_wait();
-    pic_write_slave(0x10 | 0x01);
-    io_wait();
-
-    pic_write_master(0x20);
-    io_wait();
-    pic_write_slave(0x28);
+    pic_write_cmd_slave(0x10 | 0x01);
     io_wait();
 
-    pic_write_master(0x04);
+    pic_write_data_master(0x20);
     io_wait();
-    pic_write_slave(0x02);
-    io_wait();
-
-    pic_write_master(0x01);
-    io_wait();
-    pic_write_slave(0x01);
+    pic_write_data_slave(0x28);
     io_wait();
 
-    pic_write_master(0xff);
+    pic_write_data_master(0x04);
     io_wait();
-    pic_write_slave(0xff);
+    pic_write_data_slave(0x02);
+    io_wait();
+
+    pic_write_data_master(0x01);
+    io_wait();
+    pic_write_data_slave(0x01);
+    io_wait();
+
+    pic_write_data_master(0xff);
+    io_wait();
+    pic_write_data_slave(0xff);
     io_wait();
 
     pic_clear_mask(0x02);
+}
+
+int pic_spurious(unsigned char idx) {
+    if(idx == 7) {
+        if(!(pic_read_master_isr() & 0x80)) return 1;
+    } else if(idx == 15) {
+        if(!(pic_read_slave_isr() & 0x80)) {
+            pic_write_cmd_master(0x20);
+            return 1;
+        }
+    }
+    return 0;
 }
--- src/drivers/pic.h
+++ src/drivers/pic.h
@@ -3,15 +3,70 @@
 #include "i386.h"
 
 static inline 
-void pic_write_master(unsigned char data){
+void pic_write_data_master(unsigned char data){
     port_byte_out(0x21, data);
 }
 
 static inline
-void pic_write_slave(unsigned char data) {
+void pic_write_data_slave(unsigned char data) {
     port_byte_out(0xa1, data);
+}
+
+static inline 
+void pic_write_cmd_master(unsigned char data){
+    port_byte_out(0x20, data);
+}
+
+static inline
+void pic_write_cmd_slave(unsigned char data) {
+    port_byte_out(0xa0, data);
+}
+
+static inline 
+unsigned char pic_read_data_master(){
+    return port_byte_in(0x21);
+}
+
+static inline
+unsigned char pic_read_data_slave() {
+    return port_byte_in(0xa1);
+}
+
+static inline 
+void pic_write_eoi(unsigned char idx) {
+    if(idx > 8){
+        pic_write_cmd_slave(0x20);
+    }
+    pic_write_cmd_master(0x20);
+}
+
+static inline 
+unsigned char pic_read_master_irr(){
+    pic_write_cmd_master(0x08 | 0x02 | 0x0);
+    return port_byte_in(0x20);
+}
+
+static inline 
+unsigned char pic_read_slave_irr(){
+    pic_write_cmd_slave(0x08 | 0x02 | 0x0);
+    return port_byte_in(0xa0);
+}
+
+
+static inline 
+unsigned char pic_read_master_isr(){
+    pic_write_cmd_master(0x08 | 0x02 | 0x1);
+    return port_byte_in(0x20);
+}
+
+static inline 
+unsigned char pic_read_slave_isr(){
+    pic_write_cmd_slave(0x08 | 0x02 | 0x1);
+    return port_byte_in(0xa0);
 }
 
 void pic_clear_mask(unsigned char idx);
 
 void pic_remap();
+
+int pic_spurious(unsigned char idx);
